d427ebc450e919dc581dfe8a8fdb54ee1fe40365
yanghui <yanghui@vfuturetec.com>
2025-01-15 16:48:49 +0800

    code

--- build/static/js/19.dc37a932.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcaterpillar=this.webpackJsonpcaterpillar||[]).push([[19],{291:function(t,e,n){"use strict";n.r(e);var c=n(10),r=n(11),i=n(16),a=n(17),u=n(0),s=n.n(u),o=n(29),p=n(2),l=function(t){Object(i.a)(n,t);var e=Object(a.a)(n);function n(){return Object(c.a)(this,n),e.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return Object(p.jsx)("div",{children:Object.keys(o).map((function(t){return Object(p.jsx)("div",{onClick:function(){o[t]()},children:t})}))})}}]),n}(s.a.Component);e.default=l}}]);
-//# sourceMappingURL=19.dc37a932.chunk.js.map

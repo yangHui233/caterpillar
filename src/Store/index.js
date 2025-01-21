bead70bd2682d77638e165e8c28ae6d242faf190
yanghui <yanghui@vfuturetec.com>
2025-01-21 18:18:08 +0800

    code

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,8 +2,20 @@ import { createStore } from 'redux'
 import reducers from './reducers'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage/session'
+import { retrieveLaunchParams } from '@telegram-apps/sdk'
+
+let idPre = ''
+try {
+  // 存储当前登录用户信息
+  const initialDataRaw = retrieveLaunchParams()
+  const { initData } = initialDataRaw
+  idPre = initData.user.id
+  console.log('当前登录用户信息', idPre, initData, initData.user)
+  sessionStorage.setItem('currentUserId', idPre)
+} catch (err) {}
+
 const persistConfig = {
-  key: 'root',
+  key: 'root_' + idPre,
   storage,
 }
 const persistedReducer = persistReducer(persistConfig, reducers)

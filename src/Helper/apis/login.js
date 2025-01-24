c9d1389a8c0f99eb59298e8ce30a25faf628dfac
Matzoh <eaterlow@gmail.com>
2025-01-24 20:01:14 +0800

    debug

--- src/Helper/apis/login.js
+++ src/Helper/apis/login.js
@@ -32,8 +32,9 @@ export const login = (rawData, params, isLoading) => {
     console.log(err, 'err')
   }
   // params.debug = true
-
-  const res = ajax.post('login', params, isLoading)
-  console.log(`login.req${JSON.stringify(params)}.res${JSON.stringify(res)}`)
-  return res
+  ;(async()=>{
+    const res = ajax.post('login', params, isLoading)
+    console.log(`login.req${JSON.stringify(params)}.res${JSON.stringify(res)}`)
+  })()
+  return null
 }
